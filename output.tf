a8540fab96226b507c8747b513eb977a90afbf1a
lokesh7devops <115445012+lokesh7devops@users.noreply.github.com>
2022-10-12 14:36:02 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,1 +1,18 @@
+output "az_subnet_id" {
+  description = "Subnet ID of the newly created subnet"
+  value       = azurerm_subnet.subnet.id
+}
+output "az_subnet_address_prefix" {
+  description = "Address of the newly created subnet"
+  value       = azurerm_subnet.subnet.address_prefixes
+}
+output "az_subnet_name" {
+  description = "Name of the newly created subnet"
+  value       = azurerm_subnet.subnet.name
+}
+output "az_subnet_vnet" {
+  description = "Subnet ID of the newly created subnet"
+  value       = azurerm_subnet.subnet.id
+}
+
 
