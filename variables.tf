ea12b2dd44820a96d47fcd5de38931b76eec39d4
lokesh7devops <115445012+lokesh7devops@users.noreply.github.com>
2022-10-12 14:25:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,20 @@
+variable "resource_group_name" {
+  description = "Name of the required resource group"
+  type        = string 
+}
 
+variable "subnet" {
+  description = "Name of the subnet"
+  defualt     = "cloud-subnet" 
+}
+
+variable "vnet" {
+  description = "Name of the vnet"
+  type        = string
+}
+
+variable "address_prefix" {
+  description = "Address of the subnet"
+  type        = string 
+  default     = "10.0.2.0/26"
+}
